7f81ce2c8efc81103fd05880431ea253f0a4d003
Erin Stansbury <ekstansbury@gmail.com>
2020-02-09 19:02:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :artist, :genre
+  attr_reader :name
+
+  SONGS = []
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def artists
+    @artist
+  end
+
+  
+end
